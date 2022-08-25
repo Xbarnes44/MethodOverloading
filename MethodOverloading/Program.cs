bdf082daaaa14bf4794063c1a875fef55c4def9c
Xavier Barnes <xdbcoder44@gmail.com>
2022-08-25 18:27:36 -0400

    Add project files.

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MethodOverloading
+{
+	class program
+	{
+		public static void Main(string[] args)
+		{
+
+		}
+
+		public static int Add(int num1, int num2)
+		{
+			return num1 + num2;
+		}
+		public static Decimal Add(decimal num1, decimal num2)
+		{
+			return num1 + num2;
+		}
+
+		public static string Add(int num1, int num2, bool isCheck)
+		{
+			var sum = num1 + num2;
+
+			if (isCheck == true && sum > 1)
+			{
+				return $"{sum} dollars";
+			}
+			else if (isCheck == true && sum == 1)
+			{
+				return $"{sum} dollar";
+			}
+			else if (isCheck == true && sum < 1)
+			{
+				return $"{sum} dollars";
+			}
+            else
+            {
+				return sum.ToString();
+            }
+
+		}
+	}
+}
